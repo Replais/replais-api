70b466adb8b5113b27999adfbfcd2df6645f94b2
ab-hey <ab-hey@users.noreply.github.com>
2025-11-19 14:47:27 +0530

    feat: server setup

---
+++ internal/env/env.go
@@ -0,0 +1,26 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetString(key string, defaultValue string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
+	}
+	return value
+}
+
+func GetInt(key string, defaultValue int) int {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
+	}
+	valAsInt, err := strconv.Atoi(value)
+	if err != nil {
+		return defaultValue
+	}
+	return valAsInt
+}
